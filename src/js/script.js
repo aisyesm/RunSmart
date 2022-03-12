a3231b928c24b728dc59cf70b165118205a1bd81
Ais <aisyesm@mail.ru>
2022-03-12 20:09:00 +0500

    Add smooth scroller to the top

--- src/js/script.js
+++ src/js/script.js
@@ -78,4 +78,34 @@ $(document).ready(function(){
     validateForm('#consultation-form');
     validateForm('#consultation form');
     validateForm('#order form');
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $(".pageup").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+    
+          // Store hash
+          var hash = this.hash;
+    
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 600, function(){
+    
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
 });
